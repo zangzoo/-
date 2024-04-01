5095fdce3fc225b80ddeecdf3f6bb9ee9124018f
zangzoo <20211397@sungshin.ac.kr>
2024-04-01 13:25:21 +0900

    DP문제 설명 추가

--- 8-5 DP.py
+++ 8-5 DP.py
@@ -11,8 +11,8 @@ d=[10001]*(m+1)
 
 #다이나믹 프로그래밍(바텀업)
 d[0]=0
-for i in range(n):
-    for j in range(array[i],m+1):
+for i in range(n): #n번 반복 => 화폐별로 한번씩
+    for j in range(array[i],m+1): #한 화폐일때, 가치값을 만드는 방법 계산을 위해
         if d[j-array[i]]!=10001: #(i-k)원을 만드는 방법이 존재하는 경우
             d[j]=min(d[j],d[j-array[i]]+1)
 #계산된 결과 출력
